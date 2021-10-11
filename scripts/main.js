36e60b7b9e69509cfcfea88009e13ed84378c587
InhumanPwnage <julianbrincat@hotmail.com>
2021-10-11 23:19:06 +0200

    Added more JS functionality

--- scripts/main.js
+++ scripts/main.js
@@ -4,14 +4,37 @@
 _showingPlayerPanel = false;
 _gameId = -1;
 
-function setGame(gameId) {
+function setGame(gameId) 
+{
+    if(this._gameId != gameId) {
+        $('#errors').replaceClass('display-inherit', 'display-none');
+    }
+
     this._gameId = gameId;
+
+    this.loadPlayers();
 }
 
 function loadPlayers() {
-
+    this.showHidePlayers();
 }
 
-function showHidePlayer() {
+function showHidePlayers() {
     this._showingPlayerPanel = !this._showingPlayerPanel;
+
+    if(this._showingPlayerPanel) 
+    {
+        $('#playerPanel').replaceClass('display-none','display-inherit');
+    }
+    else 
+    {
+        $('#playerPanel').replaceClass('display-inherit','display-none');
+    }
 }
+
+//utility function to replace CSS class https://stackoverflow.com/a/37297740
+(function ($) {
+    $.fn.replaceClass = function (pFromClass, pToClass) {
+        return this.removeClass(pFromClass).addClass(pToClass);
+    };
+}(jQuery));
